33b79ca00e72ab52db7381983ad453a36c77c1f2
Shurfik <vladimir.i.ovcharov@gmail.com>
2022-06-21 22:41:20 +0300

    HW03

---
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -0,0 +1,95 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.bridge.SLF4JBridgeHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import ru.javawebinar.topjava.MealTestData;
+import ru.javawebinar.topjava.model.AbstractBaseEntity;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import java.time.LocalDateTime;
+
+import static ru.javawebinar.topjava.MealTestData.*;
+import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
+import static ru.javawebinar.topjava.UserTestData.USER_ID;
+
+@ContextConfiguration({
+        "classpath:spring/spring-app.xml",
+        "classpath:spring/spring-db.xml"
+})
+@RunWith(SpringJUnit4ClassRunner.class)
+@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
+public class MealServiceTest {
+
+    static {
+        // Only for postgres driver logging
+        // It uses java.util.logging and logged via jul-to-slf4j bridge
+        SLF4JBridgeHandler.install();
+    }
+
+    @Autowired
+    private MealService service;
+
+    @Test
+    public void delete() throws Exception {
+        service.delete(USER_MEAL_ID, USER_ID);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void deleteNotFound() throws Exception {
+        service.delete(1, USER_ID);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void deleteNotOwn() throws Exception {
+        service.delete(USER_MEAL_ID, ADMIN_ID);
+    }
+
+    @Test
+    public void create() throws Exception {
+        Meal insertedMeal = new Meal(LocalDateTime.now(), "NEW", 1000);
+        service.create(insertedMeal, ADMIN_ID);
+        int id = AbstractBaseEntity.START_SEQ + 10;
+        insertedMeal.setId(id);
+        MealTestData.assertMatch(service.get(id, ADMIN_ID), insertedMeal);
+    }
+
+    @Test
+    public void get() throws Exception {
+        Meal meal = service.get(MEALS.get(1).getId(), USER_ID);
+        MealTestData.assertMatch(meal, MEALS.get(1));
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void getNotFound() throws Exception {
+        service.get(1, USER_ID);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void getNotOwn() throws Exception {
+        service.get(USER_MEAL_ID, ADMIN_ID);
+    }
+
+    @Test
+    public void update() throws Exception {
+        service.update(ADMIN_MEALS.get(0), ADMIN_ID);
+        MealTestData.assertMatch(service.get(ADMIN_MEALS.get(0).getId(), ADMIN_ID), ADMIN_MEALS.get(0));
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void updateNotFound() throws Exception {
+        service.update(MEAL1, ADMIN_ID);
+    }
+
+    @Test
+    public void getAll() throws Exception {
+        assertMatch(service.getAll(USER_ID), MEALS);
+    }
+
+}
